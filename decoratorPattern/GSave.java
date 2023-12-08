b073ae0c3e92615a82f0de9b3a95365094030c97
Ron Anthony Sy <142371703+rnnthnysy@users.noreply.github.com>
2023-12-08 17:48:39 +0800

    decoratorPattern java codes here

---
+++ decoratorPattern/GSave.java
@@ -0,0 +1,43 @@
+package decoratorPattern;
+
+public class GSave implements BankAccountDecorator{
+    private BankAccount bankAccount;
+    public GSave(BankAccount bankAccount){
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public String showAccountType() {
+        return "GSave";
+    }
+
+    @Override
+    public double getInterestRate() {
+        return 0.025;
+    }
+
+    @Override
+    public double getBalance() {
+        return bankAccount.getBalance();
+    }
+
+    @Override
+    public String showBenefits() {
+        return bankAccount.showBenefits() + ", Gcash Transfer";
+    }
+
+    @Override
+    public double computeBalanceWithInterest() {
+        return bankAccount.getBalance() * (1 + getInterestRate());
+    }
+
+    @Override
+    public String showInfo() {
+        return bankAccount.showInfo();
+    }
+
+    @Override
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+}
